30bbd85f54a52514625d0bb503753d0f56e43b4e
Pierii <pierxz@outlook.com>
2026-09-10 18:17:36 -0300

    feat: melhorado CSS e animações
    - Animações de faixas de VHS e film grain adicionadas
    - CSS atualiazdo
    - Corrigido bug da navbar
    - Feito script para abrir janela de arquivos

--- js/script.js
+++ js/script.js
@@ -5,17 +5,24 @@ const menuArrow = document.getElementById("menu-arrow");
 // abrir e fechar o menu
 menuButton.addEventListener("click", (event) => {
     event.stopPropagation();
-   
+
     dropdown.classList.toggle("open");
-    
-    menuArrow.style.transform = 
-    dropdown.classList.contains("open") ? "rotate(180deg)" : "rotate(0deg)";
+
+    menuArrow.style.transform =
+        dropdown.classList.contains("open") ? "rotate(180deg)" : "rotate(0deg)";
 });
 
-// fecha se clicar fora
-
+// fecha se clicar fora 
 document.addEventListener("click", () => {
     dropdown.classList.remove("open");
 
     menuArrow.style.transform = "rotate(0deg)";
+});
+
+// abre a janela para seleção de arquivos
+const selectButton = document.querySelector(".select-button");
+const pdfInput = document.getElementById("pdf-input");
+
+selectButton.addEventListener("click", () => {
+    pdfInput.click();
 });
